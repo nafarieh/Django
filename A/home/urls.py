895fec8f951233fc42a3a08284311b788557e0b1
zahranafarieh@aimedic.co <zahranafarieh@aimedic.co>
2023-09-23 00:01:21 +0330

    final session 14

--- A/home/urls.py
+++ A/home/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name= 'home'),
     # path('hello/', views.say_hello),
-    path('details/<int:todo_id>/',views.detail, name= 'details')
+    path('details/<int:todo_id>/',views.detail, name= 'details'),
+    path('delete/<int:todo_id>/', views.delete, name='delete' )
 ]
