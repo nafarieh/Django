5329c698cb1d1f581685fe266f6e9a03906630c1
zahranafarieh@aimedic.co <zahranafarieh@aimedic.co>
2023-09-26 22:40:43 +0330

    final session 21

--- A/accounts/views.py
+++ A/accounts/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+# from django.http import HttpResponse
+from .forms import UserRegistrationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def user_register(request):
-    return HttpResponse('User registration page')
+    # return HttpResponse('User registration page')
+    if request.method == "POST" :
+        # pass
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            # pass
+            cd = form.cleaned_data
+            user= User.objects.create_user(cd['username'], cd['email'], cd['password'])
+            user.first_name= cd['first_name']#'amirbig'
+            user.last_name= cd['last_name']#'bigderloo'
+            user.save()
+            messages.success(request, 'user registerd successfully', 'success')
+            return redirect('home')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'register.html',{'form':form})
