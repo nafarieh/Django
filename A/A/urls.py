efcbee5b565f87aa04a91414beeed387617e1c19
zahranafarieh@aimedic.co <zahranafarieh@aimedic.co>
2023-09-26 20:41:30 +0330

    final session 20

--- A/A/urls.py
+++ A/A/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('home.urls')),
     path('', include('home.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
