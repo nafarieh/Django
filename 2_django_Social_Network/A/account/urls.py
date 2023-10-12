1fe3b8030811b3d5c36fd522c32585a3f982854e
zahranafarieh@aimedic.co <zahranafarieh@aimedic.co>
2023-10-12 22:35:00 +0330

    Session_6_Social_Network

--- 2_django_Social_Network/A/account/urls.py
+++ 2_django_Social_Network/A/account/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
-
+app_name= 'account'
 urlpatterns=[
     path('register/', views.RegisterView.as_view(),name='user_register')
+    # path('register/', views.RegisterView.as_view(), name='home')
+
 ]
