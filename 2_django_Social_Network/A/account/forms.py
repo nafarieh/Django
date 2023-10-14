5b06d007bf38d024a44e37162c10965994b341a1
zahranafarieh@aimedic.co <zahranafarieh@aimedic.co>
2023-10-14 15:42:55 +0330

    session_8_Social_Network

--- 2_django_Social_Network/A/account/forms.py
+++ 2_django_Social_Network/A/account/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(widget= forms.TextInput(attrs={'class':'form-control'}))
     email = forms.EmailField(widget= forms.EmailInput(attrs={'class':'form-control'}))
-    password = forms.CharField(widget= forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'your password'}))
+    password1 = forms.CharField(label = 'password',widget= forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'your password'}))
+    password2 = forms.CharField(label = 'confirm password',widget= forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'your password'}))
 
     def clean_email(self):
         email = self.cleaned_data['email']
@@ -12,6 +14,17 @@ class UserRegistrationForm(forms.Form):
         if user:
             raise ValidationError('this email already exists')
         return email
+
+    def clean(self):
+        cd = super().clean()
+        p1 = cd.get('password1')
+        p2 = cd.get('password2')
+
+        if p1 and p2 and p2 != p2:
+            raise ValidationError('password must match')
+
+
+
 
 
 
