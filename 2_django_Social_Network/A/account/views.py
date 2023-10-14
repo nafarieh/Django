7b808f0023f9de3e27032db71c2b2bf01d4be908
zahranafarieh@aimedic.co <zahranafarieh@aimedic.co>
2023-10-14 20:12:50 +0330

    session_12_Social_Network

--- 2_django_Social_Network/A/account/views.py
+++ 2_django_Social_Network/A/account/views.py
@@ -10,7 +10,14 @@ class UserRegisterView(View):
     form_class = UserRegistrationForm
     template_name = 'account/register.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
+
     def get(self, request):
+        # if request.user.is_authenticated:
+        #     return redirect('home:home')
         # form = UserRegistrationForm()
         form = self.form_class
         return render(request, self.template_name, {'form': form})
@@ -28,6 +35,11 @@ class UserRegisterView(View):
 class UserLoginView(View):
     form_class= UserLoginForm
     template_name = 'account/login.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
 
     def get(self,request):
         form = self.form_class
