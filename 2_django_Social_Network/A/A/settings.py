ab12e015a463af4b82deb9fc0d6a356b7f797d09
zahranafarieh@aimedic.co <zahranafarieh@aimedic.co>
2023-10-15 12:36:06 +0330

    session_14_Social_Network

--- 2_django_Social_Network/A/A/settings.py
+++ 2_django_Social_Network/A/A/settings.py
@@ -123,4 +123,20 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authenticate.EmailBackend',
+]
 LOGIN_URL = '/account/Login'
+
+
+
+
+
+
+
+
+
+
+
