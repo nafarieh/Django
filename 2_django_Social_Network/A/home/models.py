0497741e1365c3da5c0143f5eb68592e16ffc836
zahranafarieh@aimedic.co <zahranafarieh@aimedic.co>
2023-10-15 16:35:46 +0330

    session_16_Social_Network

--- 2_django_Social_Network/A/home/models.py
+++ 2_django_Social_Network/A/home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+#آدرس مقاله دیتابیس
+#https://www.mongard.ir/articles/176/aunderstanding-database-relations/
+class Post(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    body = models.TextField(max_length= 500)
+    slug= models.SlugField()
+    created = models.DateTimeField(auto_now_add= True)
+    updated = models.DateTimeField(auto_now= True)
+
+    def __str__(self):
+        # return self.slug
+        return f'{self.slug} - {self.updated}'
