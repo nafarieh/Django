edaa970d0b967f3ef0b799ff405543d21371613f
zahranafarieh@aimedic.co <zahranafarieh@aimedic.co>
2023-10-16 23:34:29 +0330

    session_24_Social_Network

--- 2_django_Social_Network/A/A/settings.py
+++ 2_django_Social_Network/A/A/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$+rk=hxq1=()84&#1xtzc#2mvu!t8t%km4xfbgjj9@)d!8-7er'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- 2_django_Social_Network/A/home/views.py
+++ 2_django_Social_Network/A/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.views import View
 from .models import Post
@@ -24,12 +24,14 @@ class HomeView(View):
 
 class PostDetailView(View):
     def get(self, request, post_id , post_slug):
-        post = Post.objects.get(pk = post_id, slug = post_slug)
+        post = get_object_or_404(Post, pk=post_id, slug= post_slug)
+        # post = Post.objects.get(pk = post_id, slug = post_slug)
         return render(request, 'home/detail.html', {'post':post})
 
 class PostDeleteView(LoginRequiredMixin, View):
     def get(self, request, post_id):
-        post = Post.objects.get(pk= post_id)
+        post = get_object_or_404(Post,pk=post_id)
+        # post = Post.objects.get(pk= post_id)
         if post.user.id == request.user.id :
             post.delete()
             messages.success(request, 'post deleted successfully', 'success')
@@ -41,7 +43,8 @@ class PostUpdateView(LoginRequiredMixin, View):
     form_class = PostCreateUpdateForm
 
     def setup(self, request, *args, **kwargs):
-        self.post_instance = Post.objects.get(pk=kwargs['post_id'])
+        self.post_instance = get_object_or_404(Post, pk=kwargs['post_id'])
+        # self.post_instance = Post.objects.get(pk=kwargs['post_id'])
         return super().setup(request, *args, **kwargs)
 
     def dispatch(self, request, *args, **kwargs):
