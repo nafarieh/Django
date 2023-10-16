a5f62a6ce1fdd2d0f1c83ba8aebeb39abe396468
zahranafarieh@aimedic.co <zahranafarieh@aimedic.co>
2023-10-16 11:55:45 +0330

    session_20_Social_Network_with error in delete post

--- 2_django_Social_Network/A/home/urls.py
+++ 2_django_Social_Network/A/home/urls.py
@@ -6,5 +6,6 @@ urlpatterns= [
     path('' , views.HomeView.as_view(), name='home'),
     # path('',views.home, name='home')
     path('post/<int:post_id>/<slug:post_slug>/',views.PostDetailView.as_view(), name='post_detail'),
+    path('post/delete/<int:post_id>/' , views.PostDetailView.as_view(), name='post_delete'),
 ]
 
--- 2_django_Social_Network/A/home/views.py
+++ 2_django_Social_Network/A/home/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+
 
 # Create your views here.
 
@@ -22,10 +25,15 @@ class PostDetailView(View):
         post = Post.objects.get(pk = post_id, slug = post_slug)
         return render(request, 'home/detail.html', {'post':post})
 
-
-
-
-
+class PostDeleteView(LoginRequiredMixin, View):
+    def get(self, request, post_id, post_slug):
+        post = Post.objects.get(pk= post_id)
+        if post.user.id == request.user.id :
+            post.delete()
+            messages.success(request, 'post deleted successfully', 'success')
+        else:
+            messages.error(request, 'you cant delete this post', 'danger')
+        return redirect('home:home')
 
 
 
