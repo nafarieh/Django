6998afba159616acb29d04efe7640a718d124195
zahranafarieh@aimedic.co <zahranafarieh@aimedic.co>
2023-10-17 18:14:28 +0330

    session_32_Social_Network

--- 2_django_Social_Network/A/account/admin.py
+++ 2_django_Social_Network/A/account/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Relation, Profile
+from .models import Relation
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
