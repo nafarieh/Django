82f44917d82f6103ab93356de8800cdce40b7bf0
zahranafarieh@aimedic.co <zahranafarieh@aimedic.co>
2023-10-18 17:19:27 +0330

    session_36_Social_Network

--- 2_django_Social_Network/A/home/forms.py
+++ 2_django_Social_Network/A/home/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 class PostCreateUpdateForm(forms.ModelForm):
     class Meta:
@@ -7,7 +7,13 @@ class PostCreateUpdateForm(forms.ModelForm):
         fields = ('body',)
 
 
-
+class CommentCreateForm(forms.ModelForm):
+	class Meta:
+		model = Comment
+		fields = ('body',)
+		widgets = {
+			'body': forms.Textarea(attrs={'class':'form-control'})
+		}
 
 
 
