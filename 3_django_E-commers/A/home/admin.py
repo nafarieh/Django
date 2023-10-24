5eef3fbfde7af8a968a3440cc0469d85c7b040d4
zn.nafarieh@gmail.com <zn.nafarieh@gmail.com>
2023-10-24 22:00:38 +0330

    session_11_E-Commerce: shop models

--- 3_django_E-commers/A/home/admin.py
+++ 3_django_E-commers/A/home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Category, Product
 
 # Register your models here.
+
+admin.site.register(Category)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+	raw_id_fields = ('category',)
--- 3_django_E-commers/A/home/models.py
+++ 3_django_E-commers/A/home/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.db import models
+from django.urls import reverse
+from ckeditor.fields import RichTextField
+
 
 # Create your models here.
+class Category(models.Model):
+    sub_category = models.ForeignKey('self', on_delete=models.CASCADE, related_name='scategory', null=True, blank=True)
+    is_sub = models.BooleanField(default=False)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home:category_filter', args=[self.slug, ])
+
+
+class Product(models.Model):
+    category = models.ManyToManyField(Category, related_name='products')
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(upload_to= 'products/')
+    description = models.TextField()
+    price = models.IntegerField()  #     price = models.DecimalField(max_digits=5, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home:product_detail', args=[self.slug, ])
