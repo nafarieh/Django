91181a8dff158efda503be0ecc47d02ce6b684b2
zn.nafarieh@gmail.com <zn.nafarieh@gmail.com>
2023-10-25 13:49:11 +0330

    session_13_E-Commerce: product detail

--- 3_django_E-commers/A/home/views.py
+++ 3_django_E-commers/A/home/views.py
@@ -1,17 +1,34 @@
-from django.shortcuts import render
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
-# from .models import Product, Category
+from .models import Product, Category
+# from . import tasks
+from django.contrib import messages
+from utils import IsAdminUserMixin
+# from orders.forms import CartAddForm
 
 # Create your views here.
 
 
-# from . import tasks
-# from django.contrib import messages
-# from utils import IsAdminUserMixin
-# from orders.forms import CartAddForm
-
-
 class HomeView(View):
-	def get(self, request):
-		return render(request,'home/home.html')
+	def get(self, request, category_slug=None):
+		products = Product.objects.filter(available=True)
+		# categories = Category.objects.filter(is_sub=False)
+		# if category_slug:
+		# 	category = Category.objects.get(slug=category_slug)
+		# 	products = products.filter(category=category)
+		return render(request, 'home/home.html', {'products':products})
+
+
+
+# class ProductDetailView(View):
+# 	def get(self, request, slug):
+# 		product = get_object_or_404(Product, slug=slug)
+# 		# form = CartAddForm()
+# 		return render(request, 'home/detail.html', {'product':product})
+
+
+class ProductDetailView(View):
+	def get(self, request, slug):
+		product = get_object_or_404(Product, slug=slug)
+		# form = CartAddForm()
+		return render(request, 'home/detail.html', {'product':product})
