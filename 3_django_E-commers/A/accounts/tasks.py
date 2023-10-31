7b72910cba89261d394a4e31533b752e9dfe39d0
zn.nafarieh@gmail.com <zn.nafarieh@gmail.com>
2023-10-31 20:23:56 +0330

    session_24_E-Commerce: supervisord

--- 3_django_E-commers/A/accounts/tasks.py
+++ 3_django_E-commers/A/accounts/tasks.py
@@ -9,6 +9,9 @@ import pytz
 #Github celery
 #https://github.com/celery/django-celery-beat
 
+#supervisor commands.txt
+#https://github.com/amirbigg/oneFile-codes/blob/master/supervisor_commands.txt
+
 @shared_task
 def remove_expired_otp_codes():
     expired_time = datetime.now(tz=pytz.timezone('Asia/Tehran')) - timedelta(minutes=2)
