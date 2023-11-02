18f39146e8b4989518f6f8f37df7fdbe573bf0db
zn.nafarieh@gmail.com <zn.nafarieh@gmail.com>
2023-11-02 12:24:47 +0330

    session_29_E-Commerce: cart add

--- 3_django_E-commers/A/orders/views.py
+++ 3_django_E-commers/A/orders/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
-# from .cart import Cart
-# from home.models import Product
-# from .forms import CartAddForm, CouponApplyForm
+from .cart import Cart
+from home.models import Product
+from .forms import CartAddForm, CouponApplyForm
 # from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 # from .models import Order, OrderItem, Coupon
 # import requests
@@ -21,10 +20,13 @@ class CartView(View):
         return render(request, 'orders/cart.html')
 
 
-
 class CartAddView(View):
-	permission_required = 'orders.add_order'
+    permission_required = 'orders.add_order'
 
-	def post(self, request, product_id):
-		pass
-
+    def post(self, request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        form = CartAddForm(request.POST)
+        if form.is_valid():
+            cart.add(product, form.cleaned_data['quantity'])
+        return redirect('orders:cart')
