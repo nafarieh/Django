998dd2b3f20ea5998445356e01e6be7b3cca9a5e
zn.nafarieh@gmail.com <zn.nafarieh@gmail.com>
2023-12-12 17:15:25 +0330

    drf_session_2_1: request

--- 4_drf__Django_Rest_Framework/A/home/views.py
+++ 4_drf__Django_Rest_Framework/A/home/views.py
@@ -1,24 +1,5 @@
-from django.shortcuts import render
-from django.shortcuts import render
-from django.views import View
-
-# Create your views here.
-
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
-#
-# @api_view(['GET', 'POST', 'PUT'])
-# def home(request):
-#     return Response({'name':'amir'})
-
-
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
 class Home(APIView):
-    def get(self, request):
-        return Response({"name":"jack"})
-
-    def put(self, request):
-        pass
+    def get(self, request,name):
+        return Response({"name": name})
