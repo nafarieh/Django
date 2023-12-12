de837322b6ae5c8640f576966c109c2ec594b5e3
zn.nafarieh@gmail.com <zn.nafarieh@gmail.com>
2023-12-12 17:53:17 +0330

    drf_session_2_POST: request

--- 4_drf__Django_Rest_Framework/A/home/views.py
+++ 4_drf__Django_Rest_Framework/A/home/views.py
@@ -4,5 +4,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 class Home(APIView):
     def get(self, request):
-        name= request.query_params['name']
+        return Response({"name": 'jack'})
+
+    def post(self, request):
+        name= request.data['name']
         return Response({"name": name})
