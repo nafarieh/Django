931d873be7be8c19e1e89fe973c465d463a9f6f3
zn.nafarieh@gmail.com <zn.nafarieh@gmail.com>
2023-12-12 22:49:41 +0330

    drf_session_5: custom validators

--- 4_drf__Django_Rest_Framework/A/accounts/serializers.py
+++ 4_drf__Django_Rest_Framework/A/accounts/serializers.py
@@ -4,3 +4,8 @@ class UserRegisterSerializer(serializers.Serializer):
     username= serializers.CharField(required= True)
     email= serializers.EmailField(required= True)
     password = serializers.CharField(required= True, write_only= True)
+
+    def validate_username(self, value):
+        if value == "admin":
+            raise serializers.ValidationError('user name cant be admin')
+        return value
