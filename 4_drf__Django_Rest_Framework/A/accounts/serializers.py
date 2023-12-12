9f204ac860b064952c8ba17579846ffc56885f76
zn.nafarieh@gmail.com <zn.nafarieh@gmail.com>
2023-12-12 22:57:47 +0330

    drf_session_5_2: custom validators

--- 4_drf__Django_Rest_Framework/A/accounts/serializers.py
+++ 4_drf__Django_Rest_Framework/A/accounts/serializers.py
@@ -1,11 +1,24 @@
 from rest_framework import serializers
 
+
+def clean_email(value):
+    if 'admin' in value:
+        raise serializers.ValidationError('admin cant be in email')
+
 class UserRegisterSerializer(serializers.Serializer):
     username= serializers.CharField(required= True)
-    email= serializers.EmailField(required= True)
+    email= serializers.EmailField(required= True, validators= [clean_email])
     password = serializers.CharField(required= True, write_only= True)
+    password2 = serializers.CharField(required= True, write_only= True)
 
+    #Field Level
     def validate_username(self, value):
         if value == "admin":
             raise serializers.ValidationError('user name cant be admin')
         return value
+
+    #Object Level  # Overwrite Validate method
+    def validate(self, data):
+        if data['password'] != data['password2'] :
+            raise serializers.ValidationError('passwords must match')
+        return data
