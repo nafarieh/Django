397f6f3efe4f3e34cee0be1cced2eeed66bd483b
zn.nafarieh@gmail.com <zn.nafarieh@gmail.com>
2023-12-12 21:20:54 +0330

    drf_session_3: Serializers

--- 4_drf__Django_Rest_Framework/A/home/views.py
+++ 4_drf__Django_Rest_Framework/A/home/views.py
@@ -1,11 +1,12 @@
-# query params in http://127.0.0.1:8000/?name=zahra
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import Person
+from .serializers import PersonSerializer
+
 class Home(APIView):
     def get(self, request):
-        return Response({"name": 'jack'})
-
-    def post(self, request):
-        name= request.data['name']
-        return Response({"name": name})
+        persons= Person.objects.all()
+        # persons= Person.objects.get(name= 'amir')
+        # ser_data = PersonSerializer(instance=persons)
+        ser_data= PersonSerializer(instance=persons, many= True)
+        return Response(data= ser_data.data)
