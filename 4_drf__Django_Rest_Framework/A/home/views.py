9093a9c67595cf9f6fb5ea9a2a9d8af2564bf13a
zn.nafarieh@gmail.com <zn.nafarieh@gmail.com>
2023-12-12 16:11:55 +0330

    drf_session_1_1: response

--- 4_drf__Django_Rest_Framework/A/home/views.py
+++ 4_drf__Django_Rest_Framework/A/home/views.py
@@ -4,6 +4,9 @@ from django.views import View
 
 # Create your views here.
 
-class Home(View):
-    def get(self, request):
-        return render(request,'home/home.html')
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET', 'POST', 'PUT'])
+def home(request):
+    return Response({'name':'amir'})
