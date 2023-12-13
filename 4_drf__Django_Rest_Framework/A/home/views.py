89bbdfd89bb1ac0f53db00e31c8ff378b33fefcd
zn.nafarieh@gmail.com <zn.nafarieh@gmail.com>
2023-12-13 22:37:57 +0330

    drf_session_10: Permissions

--- 4_drf__Django_Rest_Framework/A/home/views.py
+++ 4_drf__Django_Rest_Framework/A/home/views.py
@@ -1,12 +1,17 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Person
-from .serializers import PersonSerializer
+from .serializers import PersonSerializer #, QuestionSerializer, AnswerSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .models import Person #, Question, Answer
+from rest_framework import status
+
 
 class Home(APIView):
+    # permission_classes = [IsAuthenticated,]
+    permission_classes = [IsAdminUser,]
     def get(self, request):
         persons= Person.objects.all()
-        # persons= Person.objects.get(name= 'amir')
-        # ser_data = PersonSerializer(instance=persons)
         ser_data= PersonSerializer(instance=persons, many= True)
+        # ser_data= PersonSerializer(instance=persons, many= True)
         return Response(data= ser_data.data)
