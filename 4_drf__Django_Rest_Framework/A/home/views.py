82c3a243c400a24e51d0ce61d11ac27ce97c1291
zn.nafarieh@gmail.com <zn.nafarieh@gmail.com>
2023-12-18 17:44:57 +0330

    drf_session_11_2: Read

--- 4_drf__Django_Rest_Framework/A/home/views.py
+++ 4_drf__Django_Rest_Framework/A/home/views.py
@@ -28,33 +28,16 @@ class QuestionListView(APIView):
         return Response(srz_data.data, status= status.HTTP_200_OK)
 
 class QuestionCreateView(APIView):
-    """
-        Create new question
-    """
+    def get(self, request):
+        questions = Question.objects.all()
+        srz_data = QuestionSerializer(instance=questions, many=True)
+        return Response(srz_data.data, status=status.HTTP_200_OK)
+
     def post(self, request):
-        srz_data= QuestionSerializer(data= request.POST)
-        if srz_data.is_valid():
-            srz_data.save()
-            return Response(srz_data.data, status=status.HTTP_201_CREATED)
-        return Response(srz_data.errors, status=status.HTTP_400_BAD_REQUEST)
+        pass
 
-class QuestionUpdateView(APIView):
-    """
-    Update question
-    """
-    def put(self, request, pk): #update
-        question = Question.objects.get(pk=pk)
-        srz_data= QuestionSerializer(instance=question, data= request.data, partial= True)
-        if srz_data.is_valid():
-            srz_data.save()
-            return Response(srz_data.data, status= status.HTTP_200_OK)
-        return Response(srz_data.errors, status= status.HTTP_400_BAD_REQUEST)
+    def put(self, request, pk):
+        pass
 
-class QuestionDeleteView(APIView):
-    """
-    delete question
-    """
     def delete(self, request, pk):
-        question = Question.objects.get(pk=pk)
-        question.delete()
-        return Response({'message': 'question deleted'}, data= status.HTTP_200_OK)
+        pass
